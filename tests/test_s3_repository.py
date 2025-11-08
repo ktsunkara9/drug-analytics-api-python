1de76d991a05ad66253be3606181216982abc906
Sunkara <krishnateja.sunkara@delta.com>
2025-11-08 17:54:07 +0530

    fixed failing tests

--- tests/test_s3_repository.py
+++ tests/test_s3_repository.py
@@ -11,31 +11,31 @@ from src.repositories.s3_repository import S3Repository
 from src.core.exceptions import S3Exception
 
 
-@mock_aws
 class TestS3Repository:
     """Test suite for S3Repository."""
     
     @pytest.fixture(autouse=True)
     def setup_aws(self):
         """Setup mock AWS environment and create test bucket."""
-        os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
-        os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
-        os.environ['AWS_SECURITY_TOKEN'] = 'testing'
-        os.environ['AWS_SESSION_TOKEN'] = 'testing'
-        os.environ['S3_BUCKET_NAME'] = 'test-bucket'
-        
-        # Reload settings to pick up test environment variables
-        from src.core import config
-        config.settings = config.Settings()
-        
-        # Create test bucket
-        s3_client = boto3.client('s3', region_name='us-east-1')
-        s3_client.create_bucket(Bucket='test-bucket')
-        
-        yield
-        
-        # Cleanup
-        del os.environ['S3_BUCKET_NAME']
+        with mock_aws():
+            os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
+            os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
+            os.environ['AWS_SECURITY_TOKEN'] = 'testing'
+            os.environ['AWS_SESSION_TOKEN'] = 'testing'
+            os.environ['S3_BUCKET_NAME'] = 'test-bucket'
+            
+            # Reload settings to pick up test environment variables
+            from src.core import config
+            config.settings = config.Settings()
+            
+            # Create test bucket
+            s3_client = boto3.client('s3', region_name='us-east-1')
+            s3_client.create_bucket(Bucket='test-bucket')
+            
+            yield
+            
+            # Cleanup
+            del os.environ['S3_BUCKET_NAME']
     
     def test_upload_file_success(self):
         """Test successful file upload to S3."""
